ff3bca6d0aee663dd8aaeba2b6b3f34abc7457b5
Mohamed Ezer Bouzouraa <mohamedezer.bouzouraa@insat.ucar.tn>
2026-01-31 20:54:03 +0100

    Implement Decision Tree Classifier

--- model.py
+++ model.py
@@ -5,11 +5,8 @@ from data import df
 
 X = df.drop('Country', axis=1).values
 y = df['Country'].values
-
 le = LabelEncoder()
 y_encoded = le.fit_transform(y)
-
 tree = DecisionTreeClassifier(criterion='entropy', max_depth=6, random_state=0)
 tree.fit(X, y_encoded)
-
 features = list(df.columns[1:])
